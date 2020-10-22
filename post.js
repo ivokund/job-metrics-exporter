e93c71c6d1950e52e73b330ed0c911e4e4cd3e30
Ivo Kund <ivo@pactum.com>
2020-10-22 21:18:20 +0300

    Initial commit

--- post.js
+++ post.js
@@ -5,17 +5,18 @@ const exec = require('@actions/exec');
 const postMetrics = async () => {
   const startTime = core.getState("startTime");
   const endTime = Date.now();
+
   console.log('Set start time to ' + new Date().toISOString());
+
   const diff = endTime - startTime;
-
-  console.log(`Job execution took ${diff}ms`)
-
+  console.log(`Job execution took ${diff}ms`);
   console.log({context: gh.context});
 
   const payload = {
-    job: gh.context.job,
+    job: core.getInput('job') || gh.context.job,
     repo: gh.context.repo.repo,
     duration: diff,
+    target: core.getInput('target') || null,
   };
   console.log('Metrics payload', payload);
 
