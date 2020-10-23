55c3f6c4e4a441918d94d36b3bbb530445b3d3c8
Ivo Kund <ivo@pactum.com>
2020-10-23 10:17:10 +0300

    Initial commit

--- pre.js
+++ pre.js
@@ -7,7 +7,10 @@ try {
     start = new Date(core.getInput('start'));
   }
 
-  core.saveState("startTime", +start);
+  const startTimestamp = +start;
+  console.log(startTimestamp)
+
+  core.saveState("startTime", startTimestamp);
 
   core.info(`Set start time to ${core.getState('startTime')} (${start.toISOString()})`);
 } catch (error) {
